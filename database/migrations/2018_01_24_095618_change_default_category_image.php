0f36d22d8f5fb6aa1db23c3f5582f52a00b15f12
Aztic <zetsubou97@gmail.com>
2018-01-24 05:58:06 -0400

    update default category image

---
+++ database/migrations/2018_01_24_095618_change_default_category_image.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeDefaultCategoryImage extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('categories',function(Blueprint $table){
+            $table->string('image')->default("https://u.rindou.moe/RAj3WYY3FcRmxn8bOBuc.jpg");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
